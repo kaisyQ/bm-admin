754293136e3408f8006a4bd6d3358b730178aa1f
kaisyQ <85626015+kaisyQ@users.noreply.github.com>
2024-01-20 08:31:11 +0300

    add store

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import './assets/main.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import naive from 'naive-ui'
+import naive from 'naive-ui';
+import store from './store/index';
+
 
 const app = createApp(App);
 
 app.use(router);
-
 app.use(naive);
+app.use(store);
 
 app.mount('#app');
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,4 +9,5 @@
 </template>
 
 <script setup lang="ts">
+
 </script>
